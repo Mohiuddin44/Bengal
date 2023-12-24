b8c54d8eaab9b89dff30952bf4e9797a06018ea3
Mohiuddin44 <154631529+Mohiuddin44@users.noreply.github.com>
2023-12-24 21:57:49 +0600

    Bengal Byte Booster
    website

---
+++ script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * toggle navbar
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+var swiper = new swiper(".myswiper",{
+  
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 1000, //2000ms =2s;
+  autoplayHoverPause: true,
+
+  effect: "coverflow",
+  grabcursor: true,
+  centeredSlides: true,
+  slidesperview: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideshadows: false,
+  },
+  pagination: {
+    el:  ".swiper-pagination",
+  },
+
+
+});
